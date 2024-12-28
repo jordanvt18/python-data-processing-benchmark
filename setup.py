d27eaeac7e913cfadb6caa64a673b86b6e2e7892
Jordan Villon <69180025+jordanvt18@users.noreply.github.com>
2024-12-28 13:36:37 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="data-processing-benchmark",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        'pandas>=2.0.0',
+        'polars>=0.19.0',
+        'datatable>=1.0.0',
+        'numpy>=1.20.0',
+        'jupyter>=1.0.0',
+        'matplotlib>=3.5.0',
+        'seaborn>=0.11.0',
+    ],
+    author="Tu Nombre",
+    author_email="tu.email@ejemplo.com",
+    description="Benchmark comparativo entre Pandas, Polars y Data.table",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tu-usuario/python-data-processing-benchmark",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
